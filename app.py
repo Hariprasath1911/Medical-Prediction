b5aeb9513ff78d20f373b620be3f94bbc69d5a48
Hariprasath1911 <hari07011996@gmail.com>
2024-12-31 10:00:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ if condition == 'Kidney Disease':
 
 elif condition == 'Liver Disease':
     st.header('Liver Disease Prediction')
-    col1,col2,col3 = st.columns(3)
-    col4,col5,col6,col7 = st.columns(4)
+    col1,col2,col3,col4 = st.columns(4)
+    col5,col6,col7 = st.columns(3)
     col8,col9,col10 = st.columns(3)
     
     # User input
